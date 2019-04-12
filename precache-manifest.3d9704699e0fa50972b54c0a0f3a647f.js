30666d7da6d8ff72a0f9f975222ac5ae8c35307d
inmaculada.reyes <inmaculada.reyes@softtek.com>
2019-04-12 11:39:20 +0200

    Updates

---
+++ precache-manifest.3d9704699e0fa50972b54c0a0f3a647f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfd4b6d058de9505cdad",
+    "url": "/react-portal/static/js/runtime~main.afff1fac.js"
+  },
+  {
+    "revision": "5d4087e68ae5c36c425e",
+    "url": "/react-portal/static/js/main.b5bb7da5.chunk.js"
+  },
+  {
+    "revision": "da85a86b71cec73f1646",
+    "url": "/react-portal/static/js/2.9c6e8004.chunk.js"
+  },
+  {
+    "revision": "5d4087e68ae5c36c425e",
+    "url": "/react-portal/static/css/main.e0506ef8.chunk.css"
+  },
+  {
+    "revision": "95f21f403255ef1d11dd118c1f86616e",
+    "url": "/react-portal/index.html"
+  }
+];
